8fd2c20cab2e88398d0476082c0a1060655fd309
Paulo Hernane <paulohernane10@gmail.com>
2021-10-10 10:48:42 -0300

    fix style

--- Lógica e Estrutura de Dados com JavaScript/ex_aula_01.js
+++ Lógica e Estrutura de Dados com JavaScript/ex_aula_01.js
@@ -1,14 +1,14 @@
-const NAME = "Otter" + "wise";
+const NAME = 'Otter' + 'wise'
 
-let and = true && false;
+let and = true && false
 
-let or = true || false;
+let or = true || false
 
-let not = !true;
+let not = !true
 
 console.log(`${42 * -3.14}
 ${NAME}
 ${and}
 ${or}
 ${not}
-`);
+`)
