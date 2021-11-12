1bb88080d250098a85ce4083eeddb342ef4e2b3c
Paulo Hernane <paulohernane10@gmail.com>
2021-11-11 21:57:24 -0300

    create form

--- Material Gravado/React JS/projeto-aula06/src/components/Form.js
+++ Material Gravado/React JS/projeto-aula06/src/components/Form.js
@@ -32,6 +32,7 @@ const Form = () => {
       <input type="text" {...register('description')} />
       <br />
       {errors.description?.message}
+      <br />
       <button type="submit">Postar</button>
     </form>
   );
