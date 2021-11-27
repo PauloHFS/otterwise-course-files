3663e8c621fa3c1a7b91f08aa679078ef50f08c8
Paulo Hernane <paulohernane10@gmail.com>
2021-11-27 11:32:49 -0300

    refatorando metodo de array

--- Aulas Ao Vivo/week-5/projeto-01/index.js
+++ Aulas Ao Vivo/week-5/projeto-01/index.js
@@ -2,27 +2,27 @@ import fs from 'fs';
 
 const DATA_PATH = './data/lista.csv';
 
-const data = fs.readFileSync(DATA_PATH).toString().split('\n');
+const csvFile = fs.readFileSync(DATA_PATH).toString().split('\n');
 
-// indentificar as colunas
-const columns = data[0].split('\r')[0].split(',');
+// separar as colunas dos dados
+const [columns, ...data] = csvFile;
 
-// criar os objetos com as colunas em arrays
-const dataJSON = data.reduce((dataJSON, csvLine, index) => {
-  let newDataJSON = dataJSON;
-  if (index !== 0) {
-    const [id, name, preco] = csvLine.split('\r')[0].split(',');
-    newDataJSON.push({
-      [columns[0]]: Number.parseInt(id),
-      [columns[1]]: name,
-      [columns[2]]: `R$${Number.parseFloat(preco).toFixed(2)}`.replace(
-        '.',
-        ','
-      ),
-    });
-  }
-  return newDataJSON;
-}, []);
+// colocar as colunas numa array
+const columnsArray = columns.split(',');
 
-//salvar objeto em arqvuio
-fs.writeFileSync('./data/lista.json', JSON.stringify(dataJSON));
+// mapeia a array de string dos dados para uma de objetos com coluna:valor
+const dataObject = data.map(line => {
+  const [id, name, preco] = line.split(',');
+
+  return {
+    [columnsArray[0]]: Number.parseInt(id),
+    [columnsArray[1]]: name,
+    [columnsArray[2]]: `R$${Number.parseFloat(preco).toFixed(2)}`.replace(
+      '.',
+      ','
+    ),
+  };
+});
+
+//salva a array de objetos em arquivo JSON
+fs.writeFileSync('./data/lista.json', JSON.stringify(dataObject));
